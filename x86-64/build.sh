740737631449f0e1b3351863a85c416a250dbebc
Justlook99 <72904954+Justlook99@users.noreply.github.com>
2025-01-01 22:26:37 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -23,6 +23,7 @@ PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
+PACKAGES="$PACKAGES luci-app-accesscontrol"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
