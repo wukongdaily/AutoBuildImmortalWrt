88a8d9801e0dd615351c30cda6570bc2f7966e52
Eric20242024 <166783171+Eric20242024@users.noreply.github.com>
2025-02-06 12:10:09 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -25,6 +25,8 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn_git-25.006.24857-71dbfcc_all"
+
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
