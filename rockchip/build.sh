2e37fec747f5436d2af2589d1062f03a6ea96105
tiyizzz <35622217+tiyizzz@users.noreply.github.com>
2025-02-07 10:37:50 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -51,7 +51,6 @@ PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-eqos-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-usb-printer-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-mjpg-streamer-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-wechatpush-zh-cn"
 
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
