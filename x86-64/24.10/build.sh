89b356e631f9dea58d70a0cd381a0157b8207563
Liao Kai <q3boy1@gmail.com>
2025-03-24 08:00:46 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -25,6 +25,8 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES ca-bundle"
+PACKAGES="$PACKAGES uci"
+PACKAGES="$PACKAGES libuci2"
 PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES firewall4"
 PACKAGES="$PACKAGES ip-full"
