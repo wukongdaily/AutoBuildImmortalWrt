ea7d5a0fa058d3d2e21f3855970f4f71f7eac6fc
巷子里的猫 <118711748+114225368@users.noreply.github.com>
2025-05-17 17:17:35 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -37,10 +37,6 @@ PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
-# 增加几个必备组件 方便用户安装iStore
-PACKAGES="$PACKAGES fdisk"
-PACKAGES="$PACKAGES script-utils"
-PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
