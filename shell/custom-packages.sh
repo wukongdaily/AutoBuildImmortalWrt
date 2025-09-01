7620552184258a05c55c790bfcb9b2d953d5f92e
flyhong666 <76857714+flyhong666@users.noreply.github.com>
2025-09-01 15:51:34 +0800

    Update custom-packages.sh

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -68,6 +68,6 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-ksmbd luci-i18n-ksmbd-zh-cn"
 # zerotier
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-zerotier luci-i18n-zerotier-zh-cn"
 # rustdesk-server
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-rustdesk-server luci-i18n-rustdesk-server-zh-cn"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-rustdesk-server luci-i18n-rustdesk-server-zh-cn"
 # other
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES lsof iperf3 cgroupfs-mount"
