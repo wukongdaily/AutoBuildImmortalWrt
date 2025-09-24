2cbe537c2fe906f2cc37cb31eeb561609e49b808
iTeches <ptp2860@gmail.com>
2025-09-24 20:06:05 +0800

    Update 99-custom.sh
    Remove Firewall settings

--- glinet/99-custom.sh
+++ glinet/99-custom.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # 该脚本为immortalwrt首次启动时 运行的脚本 即 /etc/uci-defaults/99-custom.sh 也就是说该文件在路由器内 重启后消失 只运行一次
 # 设置默认防火墙规则，方便虚拟机首次访问 WebUI
-LOGFILE="/etc/config/uci-defaults-log.txt"
-uci set firewall.@zone[1].input='ACCEPT'
+#LOGFILE="/etc/config/uci-defaults-log.txt"
+#uci set firewall.@zone[1].input='ACCEPT'
 
 # 设置主机名映射，解决安卓原生 TV 无法联网的问题
 uci add dhcp domain
