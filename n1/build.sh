badc64bdeb8f1506df7948cd4fe4b48661f28cc5
yxs123321 <53416197+yxs123321@users.noreply.github.com>
2025-10-25 22:33:52 +0800

    Update build.sh

--- n1/build.sh
+++ n1/build.sh
@@ -36,7 +36,6 @@ PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-smartdns-zh-cn"
 PACKAGES="$PACKAGES hysteria"
-PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
