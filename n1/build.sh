005c994c43d86bef8ab870928acacf6d965b614d
yxs123321 <53416197+yxs123321@users.noreply.github.com>
2025-10-25 20:40:11 +0800

    Update build.sh

--- n1/build.sh
+++ n1/build.sh
@@ -35,6 +35,9 @@ PACKAGES="$PACKAGES usbutils"
 PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-smartdns-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+PACKAGES="$PACKAGES hysteria"
+PACKAGES="$PACKAGES luci-app-openclash"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
