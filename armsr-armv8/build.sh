b384f05a5fcd8860b8330070b40b44be89af2c19
Leed84568l <leed84568@gmail.com>
2025-11-16 18:20:40 +0800

    Remove luci-app-store from build script
    Removed 'luci-app-store' from the list of packages.

--- armsr-armv8/build.sh
+++ armsr-armv8/build.sh
@@ -69,7 +69,6 @@ PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 PACKAGES="$PACKAGES luci-app-netmap"
 PACKAGES="$PACKAGES luci-app-ddns"
 PACKAGES="$PACKAGES luci-app-wireguard"
-PACKAGES="$PACKAGES luci-app-store"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
