4a6260b0e60dfc884c7fc0fbe77c34f7c5f2867b
Leed84568l <leed84568@gmail.com>
2025-11-16 18:35:26 +0800

    Rename netmap app to natmap and update wireguard

--- armsr-armv8/build.sh
+++ armsr-armv8/build.sh
@@ -66,9 +66,9 @@ PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
-PACKAGES="$PACKAGES luci-app-netmap"
+PACKAGES="$PACKAGES luci-app-natmap"
 PACKAGES="$PACKAGES luci-app-ddns"
-PACKAGES="$PACKAGES luci-app-wireguard"
+PACKAGES="$PACKAGES luci-proto-wireguard"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
