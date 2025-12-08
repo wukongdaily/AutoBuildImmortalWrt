7a52047a381399ff055f5c06c57101be06225924
shaer0123 <739747789@qq.com>
2025-12-08 19:07:25 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -1,5 +1,8 @@
+
 #!/bin/bash
 # Log file for debugging
+source shell/custom-packages.sh
+echo "第三方软件包: $CUSTOM_PACKAGES"
 LOGFILE="/tmp/uci-defaults-log.txt"
 echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
 echo "编译固件大小为: $PROFILE MB"
@@ -8,7 +11,7 @@ echo "Include Docker: $INCLUDE_DOCKER"
 echo "Create pppoe-settings"
 mkdir -p  /home/build/immortalwrt/files/etc/config
 
- 创建pppoe配置文件 yml传入环境变量ENABLE_PPPOE等 写入配置文件 供99-custom.sh读取
+# 创建pppoe配置文件 yml传入环境变量ENABLE_PPPOE等 写入配置文件 供99-custom.sh读取
 cat << EOF > /home/build/immortalwrt/files/etc/config/pppoe-settings
 enable_pppoe=${ENABLE_PPPOE}
 pppoe_account=${PPPOE_ACCOUNT}
@@ -18,28 +21,34 @@ EOF
 echo "cat pppoe-settings"
 cat /home/build/immortalwrt/files/etc/config/pppoe-settings
 
- ============= 同步第三方插件库==============
- 下载 run 文件仓库
-echo "🔄 Cloning run file repo..."
-git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
+if [ -z "$CUSTOM_PACKAGES" ]; then
+  echo "⚪️ 未选择 任何第三方软件包"
+else
+  # ============= 同步第三方插件库==============
+  # 正在同步第三方软件仓库
+  echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
+  git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
 
- 拷贝 run/x86 下所有 run 文件和ipk文件 到 extra-packages 目录
-mkdir -p /home/build/immortalwrt/extra-packages
-cp -r /tmp/store-run-repo/run/x86/* /home/build/immortalwrt/extra-packages/
+  # 拷贝 run/x86 下所有 run 文件和ipk文件 到 extra-packages 目录
+  mkdir -p /home/build/immortalwrt/extra-packages
+  cp -r /tmp/store-run-repo/run/x86/* /home/build/immortalwrt/extra-packages/
 
-echo "✅ Run files copied to extra-packages:"
-ls -lh /home/build/immortalwrt/extra-packages/*.run
- 解压并拷贝ipk到packages目录
-sh prepare-packages.sh
-ls -lah /home/build/immortalwrt/packages/
+  echo "✅ Run files copied to extra-packages:"
+  ls -lh /home/build/immortalwrt/extra-packages/*.run
+  # 解压并拷贝ipk到packages目录
+  sh shell/prepare-packages.sh
+  ls -lah /home/build/immortalwrt/packages/
+fi
 
- 输出调试信息
+# 输出调试信息
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建..."
- 定义所需安装的包列表 下列插件你都可以自行删减
+# 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
+PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
@@ -49,32 +58,11 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
- 静态文件服务器dufs(推荐)
+# 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
-
-============= imm仓库外的第三方插件==============
- ============= 若启用 则打开注释 ================
- istore商店
-PACKAGES="$PACKAGES luci-app-store"
- 首页和网络向导
-PACKAGES="$PACKAGES luci-i18n-quickstart-zh-cn"
- 去广告adghome
-PACKAGES="$PACKAGES luci-app-adguardhome"
-代理相关
-PACKAGES="$PACKAGES luci-app-ssr-plus"
-PACKAGES="$PACKAGES luci-app-passwall2"
-PACKAGES="$PACKAGES luci-i18n-nikki-zh-cn"
- VPN
-PACKAGES="$PACKAGES luci-app-tailscale"
-PACKAGES="$PACKAGES luci-i18n-tailscale-zh-cn"
- 分区扩容 by sirpdboy 
-PACKAGES="$PACKAGES luci-app-partexp"
-PACKAGES="$PACKAGES luci-i18n-partexp-zh-cn"
- 酷猫主题 by sirpdboy 
-ACKAGES="$PACKAGES luci-theme-kucat"
- 网络测速 by sirpdboy 
-PACKAGES="$PACKAGES luci-app-netspeedtest"
-PACKAGES="$PACKAGES luci-i18n-netspeedtest-zh-cn"
+# ======== shell/custom-packages.sh =======
+# 合并imm仓库以外的第三方插件
+PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
@@ -86,18 +74,18 @@ fi
 if echo "$PACKAGES" | grep -q "luci-app-openclash"; then
     echo "✅ 已选择 luci-app-openclash，添加 openclash core"
     mkdir -p files/etc/openclash/core
-     Download clash_meta
+    # Download clash_meta
     META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
     wget -qO- $META_URL | tar xOvz > files/etc/openclash/core/clash_meta
     chmod +x files/etc/openclash/core/clash_meta
-     Download GeoIP and GeoSite
+    # Download GeoIP and GeoSite
     wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -O files/etc/openclash/GeoIP.dat
     wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -O files/etc/openclash/GeoSite.dat
 else
     echo "⚪️ 未选择 luci-app-openclash"
 fi
 
- 构建镜像
+# 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
 echo "$PACKAGES"
 
