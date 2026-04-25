d2084c14b4618140d9ac3113ec3529286ce02721
llgzf <163733299+llgzf@users.noreply.github.com>
2026-04-26 07:21:55 +0800

    Add additional packages to build script

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -59,6 +59,11 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+PACKAGES="$PACKAGES luci-app-mwan3"
+PACKAGES="$PACKAGES luci-app-sqm"
+PACKAGES="$PACKAGES luci-app-ddns-go"
+PACKAGES="$PACKAGES luci-app-arpbind"
+PACKAGES="$PACKAGES luci-app-autoreboot"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
