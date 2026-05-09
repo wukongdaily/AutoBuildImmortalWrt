5a0a294f6a7be4081d67b92fa96957b82b86d2ed
LimitlessTao <xtmeng1228@gmail.com>
2026-05-09 21:27:11 +0800

    feat(rockchip): 增加自动下载最新 OpenClash 客户端
    在 rockchip/build24.sh 与 build25.sh 中，当选择 luci-app-openclash 时，新增通过 GitHub Releases API 获取 latest release 的 browser_download_url 并下载客户端到 /home/build/immortalwrt/packages/。build24 下载 ipk，build25 下载 apk，同时打印下载 URL 以便调试。

--- rockchip/build24.sh
+++ rockchip/build24.sh
@@ -93,6 +93,14 @@ if echo "$PACKAGES" | grep -q "luci-app-openclash"; then
     wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -O files/etc/openclash/GeoIP.dat
     wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -O files/etc/openclash/GeoSite.dat
     wget -q https://281.281265.xyz/clash/heicat.yaml -O files/etc/openclash/config/heicat.yaml
+    # 下载最新 OpenClash Client
+    URL=$(curl -s https://api.github.com/repos/vernesong/OpenClash/releases/latest \
+      | grep "browser_download_url.*ipk" \
+      | head -n1 \
+      | cut -d '"' -f 4)
+    echo "OpenClash latest ipk: $URL"
+    wget "$URL" -P /home/build/immortalwrt/packages/
+
 else
     echo "⚪️ 未选择 luci-app-openclash"
 fi
