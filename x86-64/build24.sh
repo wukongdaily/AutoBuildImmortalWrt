d3f029947a59701bbe0920ad1db7c973f2849b11
zack-112 <53525365+zack-112@users.noreply.github.com>
2026-05-30 08:29:34 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -46,6 +46,8 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建固件..."
 # ============= imm仓库内的插件==============
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
+# 排除默认的基础版dnsmasq，避免与mosdns依赖的dnsmasq-full冲突
+PACKAGES="-dnsmasq $PACKAGES"
 PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
