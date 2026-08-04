d26b65a829f64856db9059857671ad7b0483e45c
lyon <975257591@qq.com>
2026-08-05 00:58:59 +0800

    ext4

--- sunxi-cortexa53/build24.sh
+++ sunxi-cortexa53/build24.sh
@@ -67,7 +67,7 @@ fi
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
 echo "$PACKAGES"
 
-make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$ROOTSIZE
+make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$ROOTSIZE IMAGE_TYPE=ext4
 
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
