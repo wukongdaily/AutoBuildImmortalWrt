9c7847b4a2bc55c8cd617ba8fe4fa81bc0e23235
smlemk3 <13638241+smlemk3@users.noreply.github.com>
2026-08-07 22:36:12 +0800

    Update custom-packages.sh

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -33,7 +33,7 @@
 # 新版ssrp 支持mihomo
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-nft-tproxy kmod-nft-socket xray-core naiveproxy luci-app-ssr-plus luci-i18n-ssr-plus-zh-cn"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-nft-tproxy kmod-nft-socket iptables-mod-tproxy xray-core naiveproxy shadowsocks-rust-sslocal luci-app-ssr-plus luci-i18n-ssr-plus-zh-cn"
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-nft-tproxy kmod-nft-socket iptables-mod-tproxy xray-core naiveproxy shadowsocks-rust-ss-local shadowsocksr-libev-alt luci-app-ssr-plus luci-i18n-ssr-plus-zh-cn htop nano"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-nft-tproxy kmod-nft-socket iptables-mod-tproxy xray-core naiveproxy shadowsocks-rust-ss-local shadowsocksr-libev-alt dns2tcp luci-app-ssr-plus luci-i18n-ssr-plus-zh-cn htop nano"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-nikki-zh-cn"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-nekobox"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES momo luci-app-momo luci-i18n-momo-zh-cn"
