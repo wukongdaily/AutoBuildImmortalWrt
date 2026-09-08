add5f951199117d557dc67b5f0f5479eb30b3341
zuifengguai <46639423+zuifengguai@users.noreply.github.com>
2026-09-08 14:50:58 +0800

    Add luci-app-netwizard to package list

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -56,7 +56,7 @@ PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
-
+PACKAGES="$PACKAGES luci-app-netwizard"
 # 文件管理器
 # PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # ======== shell/custom-packages.sh =======
